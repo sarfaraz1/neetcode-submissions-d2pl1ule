475968b73a604853350716faa782430f2c3d4405
sarfaraz1 <uvsarfaraz@gmail.com>
2026-04-28 16:27:04 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,54 @@
+class Solution {
+    int[] inDegree;
+
+    ArrayList<Integer> al = new ArrayList<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        inDegree = new int[numCourses];
+
+        List<List<Integer>> adj = convertToAdjancyList(numCourses, prerequisites);
+
+        for (int i = 0; i < numCourses; i++) {
+            for (int neighbour : adj.get(i)) {
+                inDegree[neighbour]++;
+            }
+        }
+
+        bfs(adj);
+
+        return al.size() == numCourses;
+    }
+
+    private void bfs(List<List<Integer>> adj) {
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0)
+                q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            al.add(node);
+            for (int neighbour : adj.get(node)) {
+                inDegree[neighbour]--;
+                if (inDegree[neighbour] == 0)
+                    q.add(neighbour);
+            }
+        }
+    }
+
+    private List<List<Integer>> convertToAdjancyList(int V, int[][] edges) {
+        List<List<Integer>> al = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            al.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            int u = edge[1];
+            int v = edge[0];
+
+            al.get(u).add(v);
+        }
+
+        return al;
+    }
+}
